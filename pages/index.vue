d9a4910f04d5d1c7111670e4559e165d031d743d
RmPillar <rpillar72@gmail.com>
2024-07-29 20:32:55 +0100

    feat: updated landing page buttn hover

--- pages/index.vue
+++ pages/index.vue
@@ -11,14 +11,13 @@ useHead({
       <h1 class="heading text-center text-zinc-800">ROBIN PILLAR</h1>
       <h2 class="subheading mb-8 text-zinc-800">CREATIVE DEVELOPER</h2>
       <a
-        class="font-biggerDisplay group relative block px-4 py-3 text-xl tracking-wider xl:px-8 xl:py-6 xl:text-4xl"
+        class="font-biggerDisplay group relative block border-2 border-zinc-800 px-4 py-3 text-xl tracking-wider transition-colors duration-500 hover:bg-zinc-800 xl:border-4 xl:px-8 xl:py-6 xl:text-4xl"
         href="mailto:robin@robinpillar.dev"
       >
         <span class="relative block overflow-hidden">
-          <span class="block transition-transform duration-500 ease-in-out group-hover:-translate-y-full"> GET IN TOUCH </span>
-          <span class="absolute left-1/2 top-full w-full -translate-x-1/2 transition-transform duration-500 ease-in-out group-hover:-translate-y-full"> GET IN TOUCH </span>
+          <span class="block text-zinc-800 transition-all duration-500 ease-in-out group-hover:-translate-y-full group-hover:text-zinc-100"> GET IN TOUCH </span>
+          <span class="absolute left-1/2 top-full w-full -translate-x-1/2 text-zinc-800 transition-all duration-500 ease-in-out group-hover:-translate-y-full group-hover:text-zinc-100"> GET IN TOUCH </span>
         </span>
-        <span class="button-border absolute border-2 border-zinc-800 transition-transform duration-500 ease-in-out group-hover:scale-110 xl:border-4" />
       </a>
     </div>
   </main>
@@ -45,26 +44,6 @@ useHead({
 
     @media screen(lg) {
       font-size: clamp(51px, 5vw, 96px);
-    }
-  }
-
-  .button-border {
-    top: -2px;
-    right: -2px;
-    bottom: -2px;
-    left: -2px;
-
-    width: calc(100% + 4px);
-    height: calc(100% + 4px);
-
-    @media screen(xl) {
-      top: -4px;
-      right: -4px;
-      bottom: -4px;
-      left: -4px;
-
-      width: calc(100% + 8px);
-      height: calc(100% + 8px);
     }
   }
 }
